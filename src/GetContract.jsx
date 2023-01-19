07666bc7d1904742be3d2d56c2f1f9645cfc41b7
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-19 08:00:35 -0300

    bug: add contract instance

--- src/GetContract.jsx
+++ src/GetContract.jsx
@@ -4,10 +4,21 @@ import React, { useState, useEffect } from 'react';
 import HelloWorld from './abis/HelloWorld.json'
 import config from './config.json'; // config
 
+let provider = new ethers.providers.Web3Provider(window.ethereum, "any")
+const network = await provider.getNetwork()
+// get accounts
+const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+console.log(accounts[0])
+// get signer
+const signer = provider.getSigner();
+
+// Javascript "version" of the smart contracts
+const helloWorld = new ethers.Contract(config[network.chainId].helloWorld.address, HelloWorld, signer)
+
 const GetContract = () => {
   const [newMessage, setNewMessage] = useState("");
 
-  //called only once
+/*   //called only once
   useEffect(async () => {
     
   }, []);
@@ -18,7 +29,7 @@ const GetContract = () => {
 
   const updateMessage = async (message) => {
 
-  };
+  }; */
 
 
   return (
